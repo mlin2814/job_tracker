43a7529d52c2760e9314a84eeb1e676642a4c3fb
Matthew <mlin2814@gmail.com>
2022-10-30 21:40:30 -0400

    Successful connection to MongDB database

---
+++ models/Jobs.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const jobSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    deadline: {
+        type: Date,
+        required: true
+    },
+    skills: [{
+        type: String,
+        default: "Python"
+    }]
+})
+
+module.exports = mongoose.model('Jobs', jobSchema)
