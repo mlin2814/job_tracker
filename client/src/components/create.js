93ff2a278100724a86293cc3b50d9f3bdf9ef072
Matthew <mlin2814@gmail.com>
2022-11-03 17:19:13 -0400

    Trying out version from MongoDB MERN stack guide

---
+++ client/src/components/create.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+ 
+export default function Create() {
+ const [form, setForm] = useState({
+   title: "",
+   company: "",
+   description: "",
+   location: "",
+   date: "",
+   skills: [""]
+ });
+ const navigate = useNavigate();
+ 
+ // These methods will update the state properties.
+ function updateForm(value) {
+   return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+ 
+ // This function will handle the submission.
+ async function onSubmit(e) {
+   e.preventDefault();
+ 
+   // When a post request is sent to the create url, we'll add a new record to the database.
+   const newPerson = { ...form };
+ 
+   await fetch("http://localhost:5000/record/add", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(newPerson),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+ 
+   setForm({ title: "", company: "", description: "", location: "", date: "", skills: [""] });
+   navigate("/");
+ }
+ 
+ // This following section will display the form that takes the input from the user.
+ return (
+   <div>
+     <h3>Create New Record</h3>
+     <form onSubmit={onSubmit}>
+       <div className="form-group">
+         <label htmlFor="title">Title</label>
+         <input
+           type="text"
+           className="form-control"
+           id="title"
+           value={form.title}
+           onChange={(e) => updateForm({ title: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="company">Company</label>
+         <input
+           type="text"
+           className="form-control"
+           id="company"
+           value={form.company}
+           onChange={(e) => updateForm({ company: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="description">Description</label>
+         <input
+           type="text"
+           className="form-control"
+           id="description"
+           value={form.description}
+           onChange={(e) => updateForm({ description: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="location">Location</label>
+         <input
+           type="text"
+           className="form-control"
+           id="location"
+           value={form.location}
+           onChange={(e) => updateForm({ location: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="date">Date</label>
+         <input
+           type="text"
+           className="form-control"
+           id="date"
+           value={form.date}
+           onChange={(e) => updateForm({ date: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="description">description</label>
+         <input
+           type="text"
+           className="form-control"
+           id="description"
+           value={form.description}
+           onChange={(e) => updateForm({ description: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="radio"
+             name="positionOptions"
+             id="positionIntern"
+             value="Intern"
+             checked={form.level === "Intern"}
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+           <label htmlFor="positionIntern" className="form-check-label">Intern</label>
+         </div>
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="radio"
+             name="positionOptions"
+             id="positionJunior"
+             value="Junior"
+             checked={form.level === "Junior"}
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+           <label htmlFor="positionJunior" className="form-check-label">Junior</label>
+         </div>
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="radio"
+             name="positionOptions"
+             id="positionSenior"
+             value="Senior"
+             checked={form.level === "Senior"}
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+           <label htmlFor="positionSenior" className="form-check-label">Senior</label>
+         </div>
+       </div>
+       <div className="form-group">
+         <input
+           type="submit"
+           value="Create person"
+           className="btn btn-primary"
+         />
+       </div>
+     </form>
+   </div>
+ );
+}
