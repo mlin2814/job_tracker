903c08150b501cb7c8002d87a118143525cb081f
Collin Sidler <cjsidler@gmail.com>
2022-11-14 10:06:07 -0800

    edit skill modal

--- frontend/src/components/SkillCard.jsx
+++ frontend/src/components/SkillCard.jsx
@@ -8,12 +8,32 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { useState } from "react";
 import useUserStore from "../stores/userStore.js";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
 
 import IconButton from "@mui/material/IconButton";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+};
+
 function SkillCard({ skill }) {
     const [anchorEl, setAnchorEl] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [newName, setNewName] = useState(skill.name);
+    const [newComfortLevel, setNewComfortLevel] = useState(skill.comfortLevel);
     const open = Boolean(anchorEl);
 
     const deleteSkill = useUserStore((state) => state.deleteSkill);
@@ -26,13 +46,30 @@ function SkillCard({ skill }) {
         setAnchorEl(null);
     }
 
+    function handleModalClose() {
+        setModalOpen(false);
+        setNewName(skill.name);
+        setNewComfortLevel(skill.comfortLevel);
+    }
+
     function handleEdit() {
-        handleClose();
+        setAnchorEl(null);
+        setModalOpen(true);
     }
 
     function handleDelete() {
         deleteSkill(skill.id);
         handleClose();
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        // addSkill(name, comfortLevel);
+        // TODO - Implement edit skill function
+        console.log(
+            `New skill! name: ${newName}, comfortLevel: ${newComfortLevel}`
+        );
+        handleModalClose();
     }
 
     return (
@@ -59,9 +96,7 @@ function SkillCard({ skill }) {
                     open={open}
                     onClose={handleClose}
                 >
-                    <MenuItem onClick={handleEdit} disabled>
-                        Edit
-                    </MenuItem>
+                    <MenuItem onClick={handleEdit}>Edit</MenuItem>
                     <MenuItem onClick={handleDelete}>Delete</MenuItem>
                 </Menu>
                 <CardContent>
@@ -70,6 +105,84 @@ function SkillCard({ skill }) {
                     </Typography>
                 </CardContent>
             </Card>
+
+            <Modal
+                open={modalOpen}
+                onClose={handleModalClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                        textAlign="center"
+                    >
+                        Edit Skill
+                    </Typography>
+
+                    <form onSubmit={handleSubmit}>
+                        <Box textAlign={"center"} mt={3}>
+                            <TextField
+                                sx={{ width: 300 }}
+                                required
+                                id="outlined-required"
+                                label="Skill name"
+                                value={newName}
+                                onChange={(e) => setNewName(e.target.value)}
+                            />
+                        </Box>
+
+                        <Typography
+                            id="modal-modal-description"
+                            sx={{ mt: 2 }}
+                            textAlign="center"
+                        >
+                            Comfort level
+                        </Typography>
+
+                        <Box display="flex" justifyContent="center">
+                            <Box sx={{ width: 300 }}>
+                                <Slider
+                                    step={1}
+                                    marks
+                                    min={0}
+                                    max={10}
+                                    value={newComfortLevel}
+                                    valueLabelDisplay="auto"
+                                    onChange={(e) =>
+                                        setNewComfortLevel(e.target.value)
+                                    }
+                                />
+                            </Box>
+                        </Box>
+
+                        <Grid
+                            container
+                            mt={2}
+                            alignItems="center"
+                            justifyContent="center"
+                            gap={4}
+                        >
+                            <Button
+                                type="submit"
+                                variant="outlined"
+                                color="success"
+                            >
+                                Save
+                            </Button>
+                            <Button
+                                variant="outlined"
+                                color="error"
+                                onClick={handleModalClose}
+                            >
+                                Cancel
+                            </Button>
+                        </Grid>
+                    </form>
+                </Box>
+            </Modal>
         </Grid>
     );
 }
